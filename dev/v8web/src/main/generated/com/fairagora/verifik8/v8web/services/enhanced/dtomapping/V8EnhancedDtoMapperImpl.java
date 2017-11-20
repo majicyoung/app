2e0810d838161064ba2bad2b5d5bb4437510a982
Yann Laurent <yann.laurent@pagre-it.com>
2017-11-20 11:55:38 +0100

    Add Fr and ES in the language control files. Fiw few display bugs.

--- dev/v8web/src/main/generated/com/fairagora/verifik8/v8web/services/enhanced/dtomapping/V8EnhancedDtoMapperImpl.java
+++ dev/v8web/src/main/generated/com/fairagora/verifik8/v8web/services/enhanced/dtomapping/V8EnhancedDtoMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2017-11-17T16:50:10+0100",
+    date = "2017-11-20T11:54:15+0100",
     comments = "version: 1.1.0.Final, compiler: javac, environment: Java 1.8.0_144 (Oracle Corporation)"
 )
 @Component
