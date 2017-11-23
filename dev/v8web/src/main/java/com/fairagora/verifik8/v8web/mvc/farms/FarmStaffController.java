84c002a336f8241f4ecf0325ee23eeba9452e144
Gregoire <gregoire@192.168.1.137>
2017-11-23 16:36:42 +0100

    Debug - delete Staff is now working:-)

--- dev/v8web/src/main/java/com/fairagora/verifik8/v8web/mvc/farms/FarmStaffController.java
+++ dev/v8web/src/main/java/com/fairagora/verifik8/v8web/mvc/farms/FarmStaffController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fairagora.verifik8.v8web.data.application.V8Page;
 import com.fairagora.verifik8.v8web.data.domain.cl.CLEntityType;
@@ -35,13 +36,36 @@ public class FarmStaffController extends AbstractV8Controller {
 		RegEntity farm = regEntityRepository.findOne(id);
 
 		preparePage(farm, mv);
-		
+
 		StaffFarmFormDto dto = new StaffFarmFormDto();
 		mv.addAttribute("staffDto", dto);
-		
+
 		return "farms/staff";
 	}
 
+	/**
+	 * 
+	 * @param id
+	 * @param staffId
+	 * @param mv
+	 * @return
+	 */
+	@RequestMapping(value = "/farm/{id}/staff/delete.html", method = RequestMethod.POST)
+	public String deleteEntityStaff(@PathVariable("id") Long id, @RequestParam("entityId") Long entityId, Model mv) {
+
+		regEntityStaffRepository.findByFarmId(id).stream().filter(s -> s.getEntity().getId().equals(entityId))
+				.findFirst().ifPresent(c -> regEntityStaffRepository.delete(c));
+
+		return "redirect:/farm/" + id + "/staff.html";
+	}
+
+	/**
+	 * 
+	 * @param id
+	 * @param dto
+	 * @param mv
+	 * @return
+	 */
 	@RequestMapping(value = "/farm/{id}/staff.html", method = RequestMethod.POST)
 	public String saveEntityStaff(@PathVariable("id") Long id, StaffFarmFormDto dto, Model mv) {
 
