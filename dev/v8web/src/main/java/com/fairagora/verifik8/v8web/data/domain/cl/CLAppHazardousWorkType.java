814eda7eb557055c4f3e7170b3c973ebae9dcd66
Yann Laurent <yann.laurent@pagre-it.com>
2018-01-24 11:29:24 +0100

    add 2 tabs for farm

--- dev/v8web/src/main/java/com/fairagora/verifik8/v8web/data/domain/cl/CLAppHazardousWorkType.java
+++ dev/v8web/src/main/java/com/fairagora/verifik8/v8web/data/domain/cl/CLAppHazardousWorkType.java
@@ -1,10 +1,39 @@
 package com.fairagora.verifik8.v8web.data.domain.cl;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "cl_app_hazardous_work_type")
 public class CLAppHazardousWorkType extends CodeListSupport {
 
+	@JoinColumn(name = "CL_ENTITY_TYPE_ID")
+	@ManyToOne(optional = true)
+	protected CLAppEntityType entityType;
+
+	public CLAppEntityType getEntityType() {
+		return entityType;
+	}
+
+	public void setEntityType(CLAppEntityType entityType) {
+		this.entityType = entityType;
+	}
+	
+
+	@JoinColumn(name = "CL_FARM_TYPE_ID")
+	@ManyToOne(optional = true)
+	protected CLFarmType farmType;
+
+	public CLFarmType getFarmType() {
+		return farmType;
+	}
+
+	public void setFarmType(CLFarmType farmType) {
+		this.farmType = farmType;
+	}
+	
+	
+	
 }
