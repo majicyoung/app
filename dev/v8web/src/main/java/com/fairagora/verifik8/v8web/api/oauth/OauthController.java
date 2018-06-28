a5d5cf48ded4a6e888ca24ca0643c1a878aa1afb
surajrai <surajrai@crossovernepal.com>
2018-06-28 11:05:11 +0545

    added logout oauth route

---
+++ dev/v8web/src/main/java/com/fairagora/verifik8/v8web/api/oauth/OauthController.java
@@ -0,0 +1,37 @@
+package com.fairagora.verifik8.v8web.api.oauth;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("api")
+public class OauthController {
+
+	@Autowired
+	private TokenStore tokenStore;
+
+	@PostMapping(value = "/logout")
+	public @ResponseBody ResponseEntity<HttpStatus> logout(HttpServletRequest request) {
+		String authHeader = request.getHeader("Authorization");
+		if (authHeader != null) {
+			try {
+				String tokenValue = authHeader.replace("Bearer", "").trim();
+				OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
+				tokenStore.removeAccessToken(accessToken);
+			} catch (Exception e) {
+				return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);
+			}
+		}
+
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+	}
+}
