310f3cc4146f1d8bfd762e73c6220c8df6b51a58
surajrai <surajrai@crossovernepal.com>
2018-06-28 17:50:10 +0545

    changed getpond method

--- dev/v8web/src/main/java/com/fairagora/verifik8/v8web/services/FarmService.java
+++ dev/v8web/src/main/java/com/fairagora/verifik8/v8web/services/FarmService.java
@@ -168,6 +168,45 @@ public class FarmService extends AbstractV8Service {
 	}
 
 	@Transactional
+	public List<RegEntityFarmPond> listAllPondsForLoggedUser(V8LoggedUser loggedUser) {
+		List<RegEntityFarmPond> r = new ArrayList<>();
+
+		SYSUser u = userRepo.findByEmail(loggedUser.getUsername());
+		if (u != null) {
+			switch (u.getRole().getCode()) {
+			case SYSRole.SADMIN:
+				r.addAll(regEntityFarmPondRepository.findAll());
+				break;
+
+			case SYSRole.coop:
+				List<RegEntityFarmDetails> farmDetails = farmDetailsRepository
+						.findByCooperativeId(u.getCooperative().getId());
+				for (RegEntityFarmDetails f : farmDetails) {
+					r.addAll(regEntityFarmPondRepository.findByFarmId(f.getEntity().getId()));
+				}
+				break;
+
+			case SYSRole.farm:
+				List<V8Farm> farmDetails1 = listFarms();
+				for (V8Farm f : farmDetails1) {
+					r.addAll(regEntityFarmPondRepository.findByFarmId(f.getId()));
+				}
+				break;
+
+			case SYSRole.country:
+				List<RegEntity> farms = regEntityRepository
+						.findByEntityTypeCodeAndNationalityId(CLAppEntityType.CODE_FARM, u.getCountry().getId());
+				for (RegEntity f : farms) {
+					r.addAll(regEntityFarmPondRepository.findByFarmId(f.getId()));
+				}
+				break;
+			}
+		}
+
+		return r;
+	}
+
+	@Transactional
 	public List<RegEntityFarmPond> listVisiblePoundsForLoggedUser(V8LoggedUser loggedUser) {
 		List<RegEntityFarmPond> r = new ArrayList<>();
 
