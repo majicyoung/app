4b6628569813158790a479ffc6d520ec51676293
gregoire BARRET <greg@smartsoftasia.com>
2018-07-09 14:53:56 +0700

    Create dt_farmq_pond_production_cycle.

---
+++ dev/v8web/src/main/java/com/fairagora/verifik8/v8web/data/repo/dt/DTFarmPondProductionCycleRepository.java
@@ -0,0 +1,7 @@
+package com.fairagora.verifik8.v8web.data.repo.dt;
+
+import com.fairagora.verifik8.v8web.data.domain.dt.DTFarmPondProductionCycle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DTFarmPondProductionCycleRepository extends JpaRepository<DTFarmPondProductionCycle, Long> {
+}
