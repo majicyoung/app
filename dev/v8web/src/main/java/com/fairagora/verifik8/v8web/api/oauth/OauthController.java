648551a62dce36023f607c3238913c40ee055c36
surajrai <surajrai@crossovernepal.com>
2018-07-19 17:36:03 +0545

    added plot activities endpoint

--- dev/v8web/src/main/java/com/fairagora/verifik8/v8web/api/oauth/OauthController.java
+++ dev/v8web/src/main/java/com/fairagora/verifik8/v8web/api/oauth/OauthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("api")
+@RequestMapping(value = {"api", "green"})
 public class OauthController {
 
 	@Autowired
