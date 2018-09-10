a436ae4dd01318e722f72ca1c38af3729500535d
Kitti <kitti@smartsoftasia.com>
2018-09-10 18:24:51 +0700

    Add password encrypt on mapping

--- dev/v8web/src/main/java/com/fairagora/verifik8/v8web/mvc/infra/dtomapping/SysUserDTOMapper.java
+++ dev/v8web/src/main/java/com/fairagora/verifik8/v8web/mvc/infra/dtomapping/SysUserDTOMapper.java
@@ -9,12 +9,18 @@ import com.fairagora.verifik8.v8web.data.domain.sys.SYSRole;
 import com.fairagora.verifik8.v8web.data.domain.sys.SYSUser;
 import com.fairagora.verifik8.v8web.mvc.users.dto.RoleFormDto;
 import com.fairagora.verifik8.v8web.mvc.users.dto.UserFormDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Mapper(componentModel = "spring", uses = EntityDtoMapper.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", uses = {EntityDtoMapper.class}, unmappedTargetPolicy = ReportingPolicy.IGNORE, imports = {BCryptPasswordEncoder.class})
 public interface SysUserDTOMapper {
+
 
 	void toDto(SYSUser user, @MappingTarget UserFormDto dto);
 
+	@Mapping(target = "password",
+			expression = "java( new BCryptPasswordEncoder(11).encode(dto.getPassword()) )")
 	void fillEntity(UserFormDto dto, @MappingTarget SYSUser user);
 
 	void toDto(SYSRole findOne, @MappingTarget RoleFormDto dto);
